09a8677acad5732a2b47db00fce410fdbb52d324
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2021-10-09 12:06:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Eureka",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS("13.0")],
     products: [
         .library(name: "Eureka", targets: ["Eureka"])
     ],
